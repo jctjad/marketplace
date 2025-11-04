acfd9f0c31c38a931e84056bb9bef32d1084c460
Alex Solano <alexsolano@Alexs-MacBook-Air-6.local>
2025-11-03 20:22:01 -0500

    implemented get_User_Item_Data()

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for
 from models import db, User, Item, Chat
+from flask import send_file
 import csv
 
 main_blueprint = Blueprint('main', __name__)
@@ -18,7 +19,12 @@ def goto_item_page():
 def goto_profile_page():
     return render_template('profile.html')
 
-@main_blueprint.route('/')
-def get_database():
-    
-    return render_template('')
+@main_blueprint.route('/export')
+def get_User_Item():
+    items = db.session.query(Item).join(User, User.id == Item.seller_id)
+    with open('users_items.csv', 'w', newline='') as csvfile:
+        csvwrite = csv.writer(csvfile, delimiter=',')
+        for i in items:
+            csvwrite.writerow([i.user.id, i.user.email, i.user.firstname, i.user.last_name, i.user.profile_image,
+                               i.user.profile_description, i.user.bookmar_items, i.user.selling_items, i.user.data_create])
+    return redirect(url_for('main.goto_browse_items_page'))
