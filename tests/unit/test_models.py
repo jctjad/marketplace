5a52b5ce29cf74268ee057c5d4b23f2667c2e121
Alex Solano <alexsolano@Alexs-MacBook-Air-6.local>
2025-12-08 20:12:25 -0500

    added pytest dependencies to yaml

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,6 +1,6 @@
 from website.models import User, Item, Chat
 
-def test_set_password():
+def test_user_set_password():
     """
     GIVEN
     WHEN
@@ -9,9 +9,9 @@ def test_set_password():
     user = User()
     user.set_password('password')
     assert user.password_hash is not None
-    assert user.password_hash is not 'password'
+    assert user.password_hash != 'password'
 
-def test_check_password():
+def test_user_check_password():
     """
     GIVEN
     WHEN
@@ -22,7 +22,7 @@ def test_check_password():
     assert user.check_password('password')
     assert not user.check_password('wrongpassword')
 
-def test_to_dict():
+def test_user_to_dict():
     """
     GIVEN
     WHEN
@@ -31,7 +31,6 @@ def test_to_dict():
     user = User(id=1, email='test@mail.com', first_name='John', last_name='Smith')
     user.set_password('password')
     user_dict = user.to_dict()
-    print(user_dict)
     assert user_dict['id'] == 1
     assert user_dict['email'] == 'test@mail.com'
     assert user_dict['first_name'] == 'John'
@@ -41,3 +40,34 @@ def test_to_dict():
     assert user_dict['bookmark_items'] is None
     assert user_dict['selling_items'] is None
     assert user_dict['date_created'] is None
+
+def test_item_to_dict():
+    """
+    GIVEN
+    WHEN
+    THEN
+    """
+    item = Item(name='item', description='selling', price='5.10', condition='new')
+    item_dict = item.to_dict()
+    assert item_dict['id'] is None
+    assert item_dict['seller_id'] is None
+    assert item_dict['name'] == 'item'
+    assert item_dict['description'] == 'selling'    
+    assert item_dict['item_photos'] is None
+    assert item_dict['price'] == '5.10'
+    assert item_dict['condition'] == 'new'
+    assert item_dict['payment_options'] == []
+    assert item_dict['bookmarked'] == False
+    assert item_dict['live_on_market'] is None
+    assert item_dict['date_created'] is None
+
+def test_item_user():
+    """
+    GIVEN
+    WHEN
+    THEN
+    """
+    user = User(email='user@mail.com')
+    item = Item(name='random item', seller=user)
+    assert item.seller == user
+    assert item.seller_id == user.id
